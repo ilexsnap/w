5f4bdb17289896fdd3e63ea718fd96678989e095
ilexsnap <ilexsnap@gmail.com>
2025-07-12 13:50:22 +0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,56 @@
+import { NeoxrBot } from 'neoxr-whatsapp-bot';
+import qrcode from 'qrcode-terminal';
+import fs from 'fs-extra';
+import { config } from './config/config.js';
+import Logger from './lib/logger.js';
+import database from './lib/database.js';
+import { utils } from './lib/utils.js';
+import pluginLoader from './lib/pluginLoader.js';
+
+class WhatsAppBot {
+  constructor() {
+    this.bot = new NeoxrBot({
+      sessionName: config.sessionName,
+      printQRInTerminal: config.connectionMethod === 'qr',
+      pairingNumber: config.pairingNumber,
+      browser: ['NeoxrBot', 'Chrome', '1.0.0']
+    });
+    
+    this.cooldowns = new Map();
+    this.isConnected = false;
+  }
+  
+  async start() {
+    Logger.info('Starting NeoxrBot...');
+    
+    // Load plugins
+    await pluginLoader.loadPlugins();
+    
+    // Initialize bot events
+    this.setupEvents();
+    
+    // Start the bot
+    await this.bot.start();
+  }
+  
+  setupEvents() {
+    // Connection events
+    this.bot.on('connection.update', this.handleConnection.bind(this));
+    this.bot.on('qr', this.handleQR.bind(this));
+    this.bot.on('pairing-code', this.handlePairingCode.bind(this));
+    
+    // Message events
+    this.bot.on('messages.upsert', this.handleMessages.bind(this));
+    this.bot.on('group-participants.update', this.handleGroupUpdate.bind(this));
+  }
+  
+  handleConnection(update) {
+    const { connection, lastDisconnect } = update;
+    
+    if (connection === 'close') {
+      this.isConnected = false;
+      Logger.warn('Connection closed, attempting to reconnect...');
+    } else if (connection === 'open') {
+      this.isConnected = true;
+      Logger.success('Connected to WhatsApp!');
+      Logger.info(`Bot Name: ${config.botNa
